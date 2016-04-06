db7d0bddd6fc0b7897af17d35d7cc71cf59d2d14
tonyzedd <tonyzed@gmail.com>
2016-04-06 11:05:10 +0100

    changed comment

--- app/src/main/java/com/example/tonyz/sample/LoginActivity.java
+++ app/src/main/java/com/example/tonyz/sample/LoginActivity.java
@@ -42,11 +42,11 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
     /**
      * Id to identity READ_CONTACTS permission request.
      */
-    private static final int REQUEST_READ_CONTACTS = 0;
+     private static final int REQUEST_READ_CONTACTS = 0;
 
     /**
      * A dummy authentication store containing known user names and passwords.
-     * TODO: remove after connecting to a real authentication system.
+     * TODO: sample code remove after connecting to a real authentication system.
      */
     private static final String[] DUMMY_CREDENTIALS = new String[]{
             "foo@example.com:hello", "bar@example.com:world"
